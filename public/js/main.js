c4098b93059f6940e355cc09bc7d13f7654a3fd9
ripter <ripter001@gmail.com>
2020-09-22 22:08:36 -0700

    Working WASM markdown test

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,22 @@
-console.log('Woot You are done!');
+import init, { to_markdown, to_cmark } from '../lib/gfm.js';
+// import init from 'https://127.0.0.1:8081/gfm.js';
+
+init().then(wasm => {
+  const el = document.getElementById('out');
+  const sample = `# Testing Markdown.
+  * Item One
+  * Item Two
+
+  Code?
+  \`\`\`
+  const code = 'neato';
+  \`\`\`
+  `;
+
+  console.log('wasm loaded', wasm);
+
+  // let result = to_markdown(sample);
+  let result = to_cmark(sample);
+  console.log('result', result);
+  el.innerHTML = result;
+});
